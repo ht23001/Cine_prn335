974c58fcfc0da38d7bbe728e2a27076e0420157f
severus <ht23001@ues.edu.sv>
2024-12-01 19:01:37 -0600

    Refactorizacion tipos, junto con tipos REST

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/Boundary/rest/server/TipoSalaResource.java
@@ -0,0 +1,2 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.Boundary.rest.server;public class TipoSalaResource {
+}
---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/Control/FrmWebSocket.java
@@ -0,0 +1,2 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.Control;public class FrmWebSocket {
+}
